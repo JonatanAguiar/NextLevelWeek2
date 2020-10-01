e2514554e14460a045d49c8f08f60ff11bacc85b
JonatanAguiar <59874556+JonatanAguiar@users.noreply.github.com>
2020-10-01 18:01:57 -0300

    GiveClassesPage mobile

---
+++ mobile/src/Pages/GiveClasses/styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#8257E5',
+        justifyContent: 'center',
+        padding: 40
+    },
+
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    
+  //  title:{
+   //     fontFamily='Archivo_700Bold',
+   // },
+
+    //description: {
+
+   // }
+
+   //<Text style={styles.title}>Quer ser um Proffy?</Text>
+     //           <Text style={styles.description}>
+       //           Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+          //      </Text>
+});
+
+export default styles;

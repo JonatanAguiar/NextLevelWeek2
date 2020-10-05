867817a2ba526fcb4c29514df63f2117e28285e0
JonatanAguiar <59874556+JonatanAguiar@users.noreply.github.com>
2020-10-05 14:27:31 -0300

    Atualizacao TeacherItem estaticamente e estilos

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import {View, Image, Text} from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import styles from './styles';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 function TeacherItem() {
     return (
@@ -14,6 +19,26 @@ function TeacherItem() {
             <View style={styles.profileInfo}>
                 <Text style={styles.name}>Jonatan Koroch de Aguiar</Text>
                 <Text style={styles.subject}>Programação Java</Text>
+            </View>
+            <Text style={styles.bio}>
+                teste teste teste
+                {'\n'}
+                teste
+            </Text>  
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/hora{'  '}
+                    <Text style={styles.priceValue}>R$ 200,00</Text>
+                </Text>
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={styles.favoriteButton}>
+                        <Image source={heartOutlineIcon}></Image>
+                    </RectButton>
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon}/>
+                        <Text style={styles.contactButtonText}>Entrar em contato</Text>
+                    </RectButton>
+                </View>
             </View>
         </View>
     );
